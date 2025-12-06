f464239c99df970de3c97a14aa1800044593eb6a
fabrizzioper <fabrizzioper@gmail.com>
2025-12-06 02:29:58 -0500

    auto-commit: cambios automáticos #6665

--- src/pages/AdminUsersPage.tsx
+++ src/pages/AdminUsersPage.tsx
@@ -291,13 +291,6 @@ export const AdminUsersPage = () => {
                       }`}>
                         {user.role}
                       </span>
-                      <span className={`px-2 py-1 rounded text-xs ${
-                        user.isActive 
-                          ? 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200' 
-                          : 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200'
-                      }`}>
-                        {user.isActive ? 'Activo' : 'Inactivo'}
-                      </span>
                     </div>
                   </div>
                 </div>
